c3ccf6342ae171787a82187fb31f6ad22e739f0e
omid-joukar <joukaromid6@gmail.com>
2020-12-23 11:19:02 +0100

    refactoring models, added Lombok to models.

--- src/main/java/omid/springframework/domain/Category.java
+++ src/main/java/omid/springframework/domain/Category.java
@@ -1,9 +1,13 @@
 package omid.springframework.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Data
+@EqualsAndHashCode(exclude = {"recipes"})
 @Entity
 public class Category {
     @Id
@@ -13,27 +17,4 @@ public class Category {
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes = new HashSet<>();
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    public void setRecipes(Set<Recipe> recipes) {
-        this.recipes = recipes;
-    }
 }
