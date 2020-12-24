3083e4f35b265246e41cbaab24e3a6ed2111d517
omid-joukar <joukaromid6@gmail.com>
2020-12-24 14:59:09 +0100

    refoctoring Junit Test for IndexController using MockMvcController.

--- src/test/java/omid/springframework/controllers/IndexControllerTest.java
+++ src/test/java/omid/springframework/controllers/IndexControllerTest.java
@@ -8,6 +8,10 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -17,6 +21,8 @@ import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest {
 
@@ -30,7 +36,13 @@ public class IndexControllerTest {
         MockitoAnnotations.initMocks(this);
         indexController = new IndexController(recipeService);
     }
-
+    @Test
+    public void testMockMvc()throws Exception{
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
     @Test
     public void getIndexPage() {
         Set<Recipe> recipes = new HashSet<>();
