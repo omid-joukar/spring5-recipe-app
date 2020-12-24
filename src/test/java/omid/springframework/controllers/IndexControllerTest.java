2ddb94ddb96d5691da1a1bbeacf0de5226359e3a
omid-joukar <joukaromid6@gmail.com>
2020-12-24 14:42:39 +0100

    refoctoring Junit Test for IndexController using ArgumentCaptor.

--- src/test/java/omid/springframework/controllers/IndexControllerTest.java
+++ src/test/java/omid/springframework/controllers/IndexControllerTest.java
@@ -1,18 +1,22 @@
 package omid.springframework.controllers;
 
+import omid.springframework.domain.Recipe;
 import omid.springframework.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class IndexControllerTest {
 
@@ -29,10 +33,22 @@ public class IndexControllerTest {
 
     @Test
     public void getIndexPage() {
+        Set<Recipe> recipes = new HashSet<>();
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1L);
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(2L);
+        recipes.add(recipe1);
+        recipes.add(recipe2);
+        when(recipeService.getRecipes()).thenReturn(recipes);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
         String viewModel = indexController.getIndexPage(model);
+
         assertEquals("index",viewModel);
         verify(recipeService,times(1)).getRecipes();
-        verify(model,times(1)).addAttribute(eq("recipes"),anySet());
+        verify(model,times(1)).addAttribute(eq("recipes"),argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2,setInController.size());
 
     }
 }
