d27538ad5ccc91f0de5d7cc5df74b48491f183d4
omid-joukar <joukaromid6@gmail.com>
2020-12-27 15:09:35 +0100

    creating new recipe added recipeform page and methods to controller.

--- src/main/java/omid/springframework/controllers/RecipeController.java
+++ src/main/java/omid/springframework/controllers/RecipeController.java
@@ -1,10 +1,10 @@
 package omid.springframework.controllers;
 
+import omid.springframework.commands.RecipeCommand;
 import omid.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -18,5 +18,16 @@ public class RecipeController {
     public String showById(@PathVariable String id,Model model){
         model.addAttribute("recipe",recipeService.findById(new Long(id)));
         return "recipe/show";
+    }
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+    @PostMapping
+    @RequestMapping("/recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:/recipe/show/"+savedCommand.getId();
     }
 }
