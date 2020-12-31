68993468e3df362693c8debff77005bcc35e53a9
omid-joukar <joukaromid6@gmail.com>
2020-12-31 09:44:02 +0100

    saving image to db.

--- src/main/java/omid/springframework/services/ImageServiceImpl.java
+++ src/main/java/omid/springframework/services/ImageServiceImpl.java
@@ -1,13 +1,39 @@
 package omid.springframework.services;
 
 import lombok.extern.slf4j.Slf4j;
+import omid.springframework.domain.Recipe;
+import omid.springframework.repositories.RecipeRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.transaction.Transactional;
+import java.io.IOException;
+
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService{
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
+    @Transactional
     public void saveImageFile(Long id, MultipartFile file) {
-        log.debug("recived a file");
+        try {
+            Recipe recipe = recipeRepository.findById(id).get();
+            Byte[] bytesObject = new Byte[file.getBytes().length];
+            int i = 0 ;
+            for (byte b : file.getBytes()){
+                bytesObject[i++] = b;
+            }
+            recipe.setImage(bytesObject);
+            recipeRepository.save(recipe);
+            log.debug("recived a file");
+        } catch (IOException e) {
+            log.error("Error occured: "+ e);
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/test/java/omid/springframework/services/ImageServiceImplTest.java
@@ -0,0 +1,42 @@
+package omid.springframework.services;
+
+import omid.springframework.domain.Recipe;
+import omid.springframework.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+public class ImageServiceImplTest {
+    @Mock
+    RecipeRepository recipeRepository;
+    ImageService imageService;
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        imageService = new ImageServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void saveImageFile() throws IOException {
+        MockMultipartFile multipartFile = new MockMultipartFile("imagefile","testing.txt","text/plain",
+        "Spring Framework Omid".getBytes());
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        when(recipeRepository.findById(anyLong())).thenReturn(Optional.of(recipe));
+        ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+        imageService.saveImageFile(1L,multipartFile);
+        verify(recipeRepository,times(1)).save(argumentCaptor.capture());
+        Recipe savedRecipe = argumentCaptor.getValue();
+        assertEquals(multipartFile.getBytes().length,savedRecipe.getImage().length);
+    }
+}
